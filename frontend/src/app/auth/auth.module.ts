54e5fb8ee425db61a8aab3ec7657c05062087f8e
renato3x <pereirarenato21@gmail.com>
2022-05-04 18:28:04 -0300

    modifications

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { MaterialModule } from '../material/material.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthenticationService } from './services/authentication.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 
 @NgModule({
@@ -13,7 +17,23 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    MaterialModule
-  ]
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: []
 })
-export class AuthModule { }
+export class AuthModule {
+  static forRoot(): ModuleWithProviders<AuthModule> {
+    return {
+      ngModule: AuthModule,
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true
+        }
+      ]
+    }
+  }
+}
--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: FormGroup = this.fb.group({
+    login: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(4) ]]
+  })
+
+  constructor(
+    private authService: AuthenticationService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  submit(): void {
+    this.authService.login(this.usuario.value)
+    .subscribe(
+      () => {
+        this.router.navigateByUrl('/funcionario')
+      }
+    )
+  }
 }
